ab417645f184fe4383b994b447364673864fefc8
eiji344 <eiji.0728.eiji@icloud.com>
2024-04-18 22:12:38 +0900

    ヘッダーの作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,20 +1,25 @@
 import { memo, FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
 import { Login } from '../components/pages/Login';
-import { Home } from '../components/pages/Home';
-import { Setting } from '../components/pages/Setting';
-import { UserManagement } from '../components/pages/UserManagement';
 import { Page404 } from '../components/pages/Page404';
+import { homeRoutes } from './HomeRoutes';
+import { HeaderLayout } from "../components/templetes/HeaderLayout";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/user_management" element={<UserManagement />} />
-      <Route path="/*" element={<Page404 />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home/*" element={
+          <HeaderLayout>
+            <Routes>
+              {homeRoutes.map((route) => (
+                <Route key={route.path} path={route.path} element={route.element} />
+              ))}
+            </Routes>
+          </HeaderLayout>
+        } />
+        <Route path="*" element={<Page404 />} />
     </Routes>
   );
 });
+
