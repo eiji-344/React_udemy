4aab97707e5b2b2532a663465a78890dc0463558
eiji344 <eiji.0728.eiji@icloud.com>
2024-04-22 22:35:46 +0900

    ユーザー詳細モーダル画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Wrap, WrapItem, Center, Spinner } from '@chakra-ui/react';
-import { memo, FC, useEffect } from 'react';
+import { Wrap, WrapItem, Center, Spinner, useDisclosure } from '@chakra-ui/react';
+import { memo, FC, useEffect, useCallback } from 'react';
 
 import { UserCard } from '../organisms/user/UserCard';
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserDetailModal } from '../organisms/user/UserDetailModal';
 
 export const UserManagement: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
 
   // マウント時に一度だけgetUsers関数を呼び出している
   useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -25,11 +29,13 @@ export const UserManagement: FC = memo(() => {
                   imageUrl="https://source.unsplash.com/random"
                   userName={user.username}
                   fullName={user.name}
+                  onClick={onClickUser}
                 />
               </WrapItem>
             ))}
       </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
